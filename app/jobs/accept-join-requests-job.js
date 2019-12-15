64d1bea6b5c24e54955057ad61b7555428303240
guidojw <guidojwdejong@hotmail.com>
2019-12-15 22:53:51 +0100

    Add cron jobs functionality

---
+++ app/jobs/accept-join-requests-job.js
@@ -0,0 +1,10 @@
+'use strict'
+const roblox = require('noblox.js')
+
+class AcceptJoinRequestsJob {
+    perform = async groupId => {
+        
+    }
+}
+
+module.exports = AcceptJoinRequestsJob
---
+++ config/cron.js
@@ -0,0 +1,9 @@
+const AcceptJoinRequestsJob = require('../app/jobs/accept-join-requests-job')
+
+module.exports = {
+  'acceptJoinRequestsJob': {
+    'cron': '*/1 * * * * *',
+    'class': AcceptJoinRequestsJob,
+    'args': [1018818]
+  }
+}
