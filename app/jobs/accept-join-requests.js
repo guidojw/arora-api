82e745788fc33c844c13f5ef9f9deca5954429a1
guidojw <guidojwdejong@hotmail.com>
2020-02-14 18:57:28 +0100

    Fix accept join requests using usernames instead of ids This apparently changed as well in noblox.js v4.5.0.

--- app/jobs/accept-join-requests.js
+++ app/jobs/accept-join-requests.js
@@ -11,25 +11,25 @@ class AcceptJoinRequestsJob {
             const exiles = await groupService.getExiles()
             const suspensions = await groupService.getSuspensions()
             const requests = await roblox.getJoinRequests(groupId)
-            for (const request of requests) {
-                const userId = await roblox.getIdFromUsername(request.username)
+            for (const request of requests.data) {
+                const userId = request.requester.userId
                 if (exiles.find(exile => exile.userId === userId)) {
-                    await roblox.handleJoinRequest(groupId, request.username, false)
-                    await (new DiscordMessageJob()).perform('log', `Declined **${request.username}**` +
-                        '\'s join request')
+                    await roblox.handleJoinRequest(groupId, userId, false)
+                    await (new DiscordMessageJob()).perform('log', `Declined **${request.requester
+                            .username}**'s join request`)
                 } else {
-                    await roblox.handleJoinRequest(groupId, request.username, true)
-                    await (new DiscordMessageJob()).perform('log', `Accepted **${request.username}**` +
-                        '\'s join request')
+                    await roblox.handleJoinRequest(groupId, userId, true)
+                    await (new DiscordMessageJob()).perform('log', `Accepted **${request.requester
+                            .username}**'s join request`)
                     if (suspensions.find(suspension => suspension.userId === userId)) {
                         await roblox.setRank(groupId, userId, 2)
-                        await (new DiscordMessageJob()).perform('log', `Promoted **` +
-                            `${request.username}** from **Customer** to **Suspended**`)
+                        await (new DiscordMessageJob()).perform('log', `Promoted **${request.requester
+                            .username}** from **Customer** to **Suspended**`)
                     }
                 }
             }
         } catch (err) {
-            console.error(err.message)
+            console.error(err)
         }
     }
 }
--- app/jobs/accept-mt-join-requests.js
+++ app/jobs/accept-mt-join-requests.js
@@ -3,22 +3,22 @@ const roblox = require('noblox.js')
 
 const DiscordMessageJob = require('./discord-message')
 
-class AcceptJoinRequestsJob {
+class AcceptMtJoinRequestsJob {
     perform = async (groupId, mtGroupId) => {
         try {
             const requests = await roblox.getJoinRequests(mtGroupId)
-            for (const request of requests) {
-                const userId = await roblox.getIdFromUsername(request.username)
+            for (const request of requests.data) {
+                const userId = request.requester.userId
                 const rank = await roblox.getRankInGroup(groupId, userId)
                 if (rank >= 100) {
-                    await roblox.handleJoinRequest(mtGroupId, request.username, true)
+                    await roblox.handleJoinRequest(mtGroupId, userId, true)
                     await roblox.setRank(mtGroupId, userId, rank)
-                    await (new DiscordMessageJob()).perform('log', `Accepted **${request.username}**` +
-                        '\'s MT join request')
+                    await (new DiscordMessageJob()).perform('log', `Accepted **${request.requester
+                            .username}**'s MT join request`)
                 } else {
-                    await roblox.handleJoinRequest(mtGroupId, request.username, false)
-                    await (new DiscordMessageJob()).perform('log', `Declined **${request.username}**` +
-                        '\'s MT join request')
+                    await roblox.handleJoinRequest(mtGroupId, userId, false)
+                    await (new DiscordMessageJob()).perform('log', `Declined **${request.requester
+                            .username}**'s MT join request`)
                 }
             }
         } catch (err) {
@@ -27,4 +27,4 @@ class AcceptJoinRequestsJob {
     }
 }
 
-module.exports = AcceptJoinRequestsJob
+module.exports = AcceptMtJoinRequestsJob
