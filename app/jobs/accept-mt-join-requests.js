11ca2d6e1593122bb95ab6554b36ba0fb397c03a
guidojw <guidojwdejong@hotmail.com>
2020-02-25 03:56:41 +0100

    Loop through join requests pages by using cursors

--- app/jobs/accept-mt-join-requests.js
+++ app/jobs/accept-mt-join-requests.js
@@ -6,21 +6,28 @@ const DiscordMessageJob = require('./discord-message')
 class AcceptMtJoinRequestsJob {
     perform = async (groupId, mtGroupId) => {
         try {
-            const requests = await roblox.getJoinRequests(mtGroupId)
-            for (const request of requests.data) {
-                const userId = request.requester.userId
-                const rank = await roblox.getRankInGroup(groupId, userId)
-                if (rank >= 100) {
-                    await roblox.handleJoinRequest(mtGroupId, userId, true)
-                    await roblox.setRank(mtGroupId, userId, rank)
-                    await (new DiscordMessageJob()).perform('log', `Accepted **${request.requester
-                            .username}**'s MT join request`)
-                } else {
-                    await roblox.handleJoinRequest(mtGroupId, userId, false)
-                    await (new DiscordMessageJob()).perform('log', `Declined **${request.requester
-                            .username}**'s MT join request`)
+            let cursor = null
+            do {
+                const requests = await roblox.getJoinRequests({
+                    group: mtGroupId,
+                    cursor: cursor
+                })
+                for (const request of requests.data) {
+                    const userId = request.requester.userId
+                    const rank = await roblox.getRankInGroup(groupId, userId)
+                    if (rank >= 100) {
+                        await roblox.handleJoinRequest(mtGroupId, userId, true)
+                        await roblox.setRank(mtGroupId, userId, rank)
+                        await (new DiscordMessageJob()).perform('log', `Accepted **${request.requester
+                                .username}**'s MT join request`)
+                    } else {
+                        await roblox.handleJoinRequest(mtGroupId, userId, false)
+                        await (new DiscordMessageJob()).perform('log', `Declined **${request.requester
+                                .username}**'s MT join request`)
+                    }
                 }
-            }
+                cursor = requests.nextPageCursor
+            } while (cursor)
         } catch (err) {
             console.error(err.message)
         }
