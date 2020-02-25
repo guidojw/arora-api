18f6b18883238d9e8428b94804573eab6913cb85
guidojw <guidojwdejong@hotmail.com>
2020-02-25 05:38:37 +0100

    Add Sentry middlewares

--- app/jobs/accept-mt-join-requests.js
+++ app/jobs/accept-mt-join-requests.js
@@ -3,35 +3,29 @@ const roblox = require('noblox.js')
 
 const DiscordMessageJob = require('./discord-message')
 
-class AcceptMtJoinRequestsJob {
+module.exports = class AcceptMtJoinRequestsJob {
     perform = async (groupId, mtGroupId) => {
-        try {
-            let cursor = null
-            do {
-                const requests = await roblox.getJoinRequests({
-                    group: mtGroupId,
-                    cursor: cursor
-                })
-                for (const request of requests.data) {
-                    const userId = request.requester.userId
-                    const rank = await roblox.getRankInGroup(groupId, userId)
-                    if (rank >= 100) {
-                        await roblox.handleJoinRequest(mtGroupId, userId, true)
-                        await roblox.setRank(mtGroupId, userId, rank)
-                        await (new DiscordMessageJob()).perform('log', `Accepted **${request.requester
-                                .username}**'s MT join request`)
-                    } else {
-                        await roblox.handleJoinRequest(mtGroupId, userId, false)
-                        await (new DiscordMessageJob()).perform('log', `Declined **${request.requester
-                                .username}**'s MT join request`)
-                    }
+        let cursor = null
+        do {
+            const requests = await roblox.getJoinRequests({
+                group: mtGroupId,
+                cursor: cursor
+            })
+            for (const request of requests.data) {
+                const userId = request.requester.userId
+                const rank = await roblox.getRankInGroup(groupId, userId)
+                if (rank >= 100) {
+                    await roblox.handleJoinRequest(mtGroupId, userId, true)
+                    await roblox.setRank(mtGroupId, userId, rank)
+                    await (new DiscordMessageJob()).perform('log', `Accepted **${request.requester
+                            .username}**'s MT join request`)
+                } else {
+                    await roblox.handleJoinRequest(mtGroupId, userId, false)
+                    await (new DiscordMessageJob()).perform('log', `Declined **${request.requester
+                            .username}**'s MT join request`)
                 }
-                cursor = requests.nextPageCursor
-            } while (cursor)
-        } catch (err) {
-            console.error(err.message)
-        }
+            }
+            cursor = requests.nextPageCursor
+        } while (cursor)
     }
 }
-
-module.exports = AcceptMtJoinRequestsJob
--- app/jobs/check-suspensions.js
+++ app/jobs/check-suspensions.js
@@ -7,35 +7,29 @@ const trelloService = require('../services/trello')
 
 const timeHelper = require('../helpers/time')
 
-class CheckSuspensionsJob {
+module.exports = class CheckSuspensionsJob {
     perform = async groupId => {
-        try {
-            const now = timeHelper.getUnix()
-            const boardId = await trelloService.getIdFromBoardName('[NS] Ongoing Suspensions')
-            const listId = await trelloService.getIdFromListName(boardId, 'Current')
-            const newListId = await trelloService.getIdFromListName(boardId, 'Done')
-            const cards = await trelloService.getCards(listId, {fields: 'name,desc'})
-            for (const card of cards) {
-                const suspension = JSON.parse(card.desc)
-                suspension.userId = parseInt(card.name)
-                let duration = suspension.duration
-                if (suspension.extended) {
-                    for (const extension of suspension.extended) {
-                        duration += extension.duration
-                    }
-                }
-                if (suspension.at + duration <= now) {
-                    await roblox.setRank(groupId, suspension.userId, suspension.rankback ? suspension.rank : 1)
-                    await trelloService.putCard(card.id, { idList: newListId })
-                    const username = await roblox.getUsernameFromId(suspension.userId)
-                    await (new DiscordMessageJob()).perform('log', `Finished **${username}**'s ` +
-                        'suspension')
+        const now = timeHelper.getUnix()
+        const boardId = await trelloService.getIdFromBoardName('[NS] Ongoing Suspensions')
+        const listId = await trelloService.getIdFromListName(boardId, 'Current')
+        const newListId = await trelloService.getIdFromListName(boardId, 'Done')
+        const cards = await trelloService.getCards(listId, {fields: 'name,desc'})
+        for (const card of cards) {
+            const suspension = JSON.parse(card.desc)
+            suspension.userId = parseInt(card.name)
+            let duration = suspension.duration
+            if (suspension.extended) {
+                for (const extension of suspension.extended) {
+                    duration += extension.duration
                 }
             }
-        } catch (err) {
-            console.error(err.message)
+            if (suspension.at + duration <= now) {
+                await roblox.setRank(groupId, suspension.userId, suspension.rankback ? suspension.rank : 1)
+                await trelloService.putCard(card.id, { idList: newListId })
+                const username = await roblox.getUsernameFromId(suspension.userId)
+                await (new DiscordMessageJob()).perform('log', `Finished **${username}**'s ` +
+                    'suspension')
+            }
         }
     }
 }
-
-module.exports = CheckSuspensionsJob
