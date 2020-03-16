718b4e87f9ebeabb981ca8d59b37e1aae4adf660
guidojw <guidojwdejong@hotmail.com>
2020-03-16 01:37:20 +0100

    Fix paths

--- app/jobs/backup.js
+++ app/jobs/backup.js
@@ -1,17 +1,20 @@
 'use strict'
 const { execFile } = require('child_process')
+const path = require('path')
 
 const databaseConfig = require('../../config/database')[process.env.NODE_ENV]
 
+const backupsPath = '~/storage/backups'
+
 module.exports = () => {
     const date = new Date()
-    const currentDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}-${date.getHours()}-${date
+    const backupName = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}-${date.getHours()}-${date
         .getMinutes()}`
-    const backupFile = `~/storage/backups/export_${currentDate}`
+    const backupFile = `${backupsPath}/export_${backupName}`
     const backupScript = `pg_dump --username=${databaseConfig.username} ${databaseConfig.database}`
 
     execFile(
-        `../../bin/backup.sh`,
+        path.resolve(__dirname, '../../bin/backup.sh'),
         [backupScript, backupFile, databaseConfig.password],
         error => {
             if (error) console.log(`exec error: ${error}`)
