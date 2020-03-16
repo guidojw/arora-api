f9b5cba81e546b57d8965c900d5f59c00c761021
Guido <guidojwdejong@hotmail.com>
2020-03-16 02:22:57 +0100

    Add success notification

--- app/jobs/backup.js
+++ app/jobs/backup.js
@@ -1,24 +1,30 @@
 'use strict'
 const { execFile } = require('child_process')
 const path = require('path')
+const discordMessageJob = require('../jobs/discord-message')
 
-const databaseConfig = require('../../config/database')[process.env.NODE_ENV]
-
-const backupsPath = '~/storage/backups'
+const databaseConfig = require('../../config/database')[process.env.NODE_ENV || 'development']
 
 module.exports = () => {
     const date = new Date()
     const backupName = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}-${date.getHours()}-${date
         .getMinutes()}`
-    const backupFile = `${backupsPath}/export_${backupName}`
+    const backupFile = `export_${backupName}`
     const backupScript = `pg_dump --username=${databaseConfig.username} ${databaseConfig.database}`
 
     execFile(
         path.resolve(__dirname, '../../bin/backup.sh'),
         [backupScript, backupFile, databaseConfig.password],
         error => {
-            if (error) console.log(`exec error: ${error}`)
-            console.log('Backup complete!')
+            if (error) {
+                throw error
+            } else {
+                discordMessageJob('backupNotification', {
+                    title: `${databaseConfig.database}--backup successful`,
+                    description: `${databaseConfig.database}--backup has been executed successfully!`,
+                    color: 65313
+                })
+            }
         }
     )
 }
--- bin/backup.sh
+++ bin/backup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-PGPASSWORD="${3}" ${1} > ./${2}
+PGPASSWORD="${3}" ${1} > ~/storage/backups/${2}
