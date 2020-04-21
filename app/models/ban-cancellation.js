f78d4ecac31f460dbab86d25c0b520b7ae778dde
guidojw <guidojwdejong@hotmail.com>
2020-04-21 05:06:23 +0200

    Linter

--- app/models/ban-cancellation.js
+++ app/models/ban-cancellation.js
@@ -1,26 +1,26 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const BanCancellation = sequelize.define('BanCancellation', {
-    authorId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    reason: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true
-      }
+    const BanCancellation = sequelize.define('BanCancellation', {
+        authorId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        reason: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        }
+    }, {})
+
+    BanCancellation.associate = models => {
+        BanCancellation.belongsTo(models.Ban, {
+            foreignKey: { allowNull: false, name: 'banId' },
+            onDelete: 'cascade',
+            hooks: true
+        })
     }
-  }, {})
 
-  BanCancellation.associate = models => {
-    BanCancellation.belongsTo(models.Ban, {
-      foreignKey: { allowNull: false, name: 'banId' },
-      onDelete: 'cascade',
-      hooks: true
-    })
-  }
-
-  return BanCancellation
+    return BanCancellation
 }
--- app/models/ban.js
+++ app/models/ban.js
@@ -1,44 +1,45 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const Ban = sequelize.define('Ban', {
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    reason: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true
-      }
-    },
-    rank: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    date: {
-      type: DataTypes.DATE,
-      allowNull: false
-    },
-    authorId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
+    const Ban = sequelize.define('Ban', {
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        reason: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        rank: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        authorId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    }, {})
+
+    Ban.associate = models => {
+        Ban.hasOne(models.BanCancellation, { foreignKey: { allowNull: false, name: 'banId' }})
     }
-  }, {})
 
-  Ban.associate = models => {
-      Ban.hasOne(models.BanCancellation, { foreignKey: { allowNull: false, name: 'banId' }})
-  }
+    Ban.loadScopes = models => {
+        Ban.addScope('defaultScope', {
+            where: { '$BanCancellation.id$': null },
+            include: [{
+                model: models.BanCancellation,
+                attributes: []
+            }]
+        })
+    }
 
-  Ban.loadScopes = models => {
-    Ban.addScope('defaultScope', {
-      where: { '$BanCancellation.id$': null },
-      include: [{
-        model: models.BanCancellation,
-        attributes: []
-      }]
-    })
-  }
-
-  return Ban
+    return Ban
 }
