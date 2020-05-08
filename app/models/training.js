a8863e9c485b70d450ddacb9fd913d5a2409da00
guidojw <guidojwdejong@hotmail.com>
2020-05-08 18:01:40 +0200

    Fix training scope's interval

--- app/models/training.js
+++ app/models/training.js
@@ -68,7 +68,7 @@ module.exports = (sequelize, DataTypes) => {
     Training.loadScopes = models => {
         Training.addScope('defaultScope', {
             where: { '$TrainingCancellation.id$': null, date: {
-                [Op.gt]: sequelize.literal('NOW() + INTERVAL \'15 minutes\'')
+                [Op.gt]: sequelize.literal('NOW() - INTERVAL \'15 minutes\'')
             }},
             include: [{
                 model: models.TrainingCancellation,
