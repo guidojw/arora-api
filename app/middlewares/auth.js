db408e5753ffbc563a006afda0f939a8620f41c1
guidojw <guidojwdejong@hotmail.com>
2020-05-16 02:51:05 +0200

    Use secret instead of token

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -13,9 +13,9 @@ exports.authenticate = (req, _res, next) => {
 
 exports.verifyTrelloWebhookRequest = (req, _res, next) => {
     const base64Digest = content => {
-        return crypto.createHmac('sha1', process.env.TRELLO_TOKEN).update(content).digest('base64')
+        return crypto.createHmac('sha1', process.env.TRELLO_SECRET).update(content).digest('base64')
     }
-    const content = JSON.stringify(req.body) + `${req.protocol}://${req.hostname}${req.baseUrl}`
+    const content = JSON.stringify(req.body) + `https://${req.hostname}${req.baseUrl}`
     const doubleHash = base64Digest(content)
     const headerHash = req.headers['x-trello-webhook']
     if (doubleHash !== headerHash) throw createError(401, 'Invalid signature.')
