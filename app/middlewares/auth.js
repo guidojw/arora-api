75714329e79d86f8afefc653db9aa39225af8054
guidojw <guidojwdejong@hotmail.com>
2020-05-23 03:29:17 +0200

    Improve middlewares

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -1,13 +1,13 @@
 'use strict'
 require('dotenv').config()
 
-const createError = require('http-errors')
 const crypto = require('crypto')
 const authService = require('../services/auth')
+const UnauthorizedError = require('../errors/unauthorized')
 
 exports.authenticate = (req, _res, next) => {
     const token = req.header('authorization').replace('Bearer ', '')
-    if (!authService.authenticate(token)) throw createError(401, 'Invalid authentication key.')
+    if (!authService.authenticate(token)) throw new UnauthorizedError('Invalid authentication key.')
     next()
 }
 
@@ -18,6 +18,6 @@ exports.verifyTrelloWebhookRequest = (req, _res, next) => {
     const content = JSON.stringify(req.body) + `https://${req.hostname}${req.baseUrl}`
     const doubleHash = base64Digest(content)
     const headerHash = req.headers['x-trello-webhook']
-    if (doubleHash !== headerHash) throw createError(401, 'Invalid signature.')
+    if (doubleHash !== headerHash) throw new UnauthorizedError('Invalid signature.')
     next()
 }
