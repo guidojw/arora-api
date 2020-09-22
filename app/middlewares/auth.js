be89b03fc64d97be4d12eadd64fe2eafc83a8529
Guido de Jong <guido@guidojw.nl>
2020-09-22 15:54:25 +0200

    Feature/WebSocket authorization (#104)
    * Add middleware for authorizing HTTP upgrade requests
    
    * Authenticate HTTP upgrade requests
    
    * Remove debug log
    
    * Rename method

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -7,8 +7,23 @@ const UnauthorizedError = require('../errors/unauthorized')
 
 exports.authenticate = (req, _res, next) => {
     const token = req.header('authorization').replace('Bearer ', '')
-    if (!authService.authenticate(token)) throw new UnauthorizedError('Invalid authentication key.')
+
+    if (!authService.authenticate(token)) {
+        throw new UnauthorizedError('Invalid authentication key.')
+    }
     next()
+}
+
+exports.authenticateWebSocketConnection = req => {
+    // Check for existence of the authorization header as these
+    // requests are not checked by express-validator
+    const token = req.headers.authorization !== undefined
+        ? req.headers.authorization.replace('Bearer ', '')
+        : undefined
+
+    if (!authService.authenticate(token)) {
+        throw new UnauthorizedError('Invalid authentication key.')
+    }
 }
 
 exports.verifyTrelloWebhookRequest = (req, _res, next) => {
@@ -18,6 +33,9 @@ exports.verifyTrelloWebhookRequest = (req, _res, next) => {
     const content = JSON.stringify(req.body) + `https://${req.hostname}${req.baseUrl}`
     const doubleHash = base64Digest(content)
     const headerHash = req.headers['x-trello-webhook']
-    if (doubleHash !== headerHash) throw new UnauthorizedError('Invalid signature.')
+
+    if (doubleHash !== headerHash) {
+        throw new UnauthorizedError('Invalid signature.')
+    }
     next()
 }
