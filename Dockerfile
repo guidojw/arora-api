20dd3d6efbaec9111d101b1c1401eb2de265a92d
guidojw <guidojwdejong@hotmail.com>
2020-10-03 22:14:41 +0200

    Dockerize app

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,10 @@
 FROM node:14
 
-ENV NODE_ENV production
-
-# Create app directory
-WORKDIR /usr/src/app
-
-# Install Node.js dependencies
+# Install dependencies
+WORKDIR /opt/app
 COPY package*.json ./
-
-USER node
-
 RUN npm install
+
 RUN npm ci --only=production
 
 # Bundle app source
--- app/models/index.js
+++ app/models/index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const Sequelize = require('sequelize')
 const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || 'development'
-const config = require(__dirname + '/../../config/database.json')[env]
+const config = require(__dirname + '/../../config/database.js')[env]
 const db = {}
 
 let sequelize
