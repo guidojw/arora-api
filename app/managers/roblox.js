b6698110646901368b7e429a619767dfdb4143d4
Guido de Jong <guido@guidojw.nl>
2020-10-19 03:57:26 +0200

    Remove double indexing (#119)

--- app/managers/roblox.js
+++ app/managers/roblox.js
@@ -19,7 +19,7 @@ exports.init = async () => {
         await client.login({ cookie: process.env.ROBLOX_COOKIE })
         console.log('Roblox account logged in!')
         const groups = await client.user.getGroups()
-        const groupIds = groups.map(group => group.group.id)
+        const groupIds = groups.map(group => group.id)
         for (const groupId of groupIds) {
             clients.authenticated[groupId] = client
         }
