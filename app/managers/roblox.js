4649608dd21af04ac9cb8549e260030629926f96
guidojw <guidojwdejong@hotmail.com>
2020-10-20 00:16:11 +0200

    Update RobloxManager

--- app/managers/roblox.js
+++ app/managers/roblox.js
@@ -14,19 +14,33 @@ let initiated = false
 exports.init = async () => {
     if (initiated) return
     initiated = true
+
     try {
-        const client = new Client({ setup: { throwHttpErrors: true }})
-        await client.login({ cookie: process.env.ROBLOX_COOKIE })
+        const client = new Client({
+            rest: {
+                // Bloxy's default requester (got) doesn't throw HTTP errors,
+                // so this custom requester is used which does.
+                requester
+            },
+            credentials: {
+                cookie: process.env.ROBLOX_COOKIE
+            }
+        })
+
+        await client.login()
         console.log('Roblox account logged in!')
+
         const groups = await client.user.getGroups()
-        const groupIds = groups.map(group => group.id)
+        const groupIds = groups.data.map(group => group.group.id)
         for (const groupId of groupIds) {
             clients.authenticated[groupId] = client
         }
+
     } catch (err) {
         console.error(err.message)
     }
-    clients.unauthenticated = new Client({ setup: { throwHttpErrors: true }})
+
+    clients.unauthenticated = new Client({ rest: { requester }})
 
     checkSuspensionsJob()
     announceTrainingsJob(robloxConfig.defaultGroup)
@@ -34,4 +48,27 @@ exports.init = async () => {
 
 exports.getClient = groupId => {
     return groupId ? clients.authenticated[groupId] : clients.unauthenticated
+}
+
+// Custom requester for Bloxy using Axios.
+async function requester(options) {
+    let result
+    try {
+        result = await axios.request(options)
+
+    } catch (err) {
+        // Map status and statusText to corresponding
+        // statusCode and message in thrown error.
+        err.statusCode = err.response.status
+        err.message = err.response.statusText
+
+        throw err
+    }
+
+    // Bloxy expects (the very odd) got's format for response data,
+    // so map two variables to keep Bloxy responseHandlers from breaking.
+    result.statusCode = result.status
+    result.body = result.data
+
+    return result
 }
