fe1967a0e54ecd4dde3f932a4f51f1a1a3eb1373
Guido de Jong <guido@guidojw.nl>
2020-11-07 02:37:18 +0100

    Remove backup job (#144)

--- app/jobs/index.js
+++ app/jobs/index.js
@@ -3,7 +3,6 @@ module.exports = {
   AcceptJoinRequestsJob: require('./accept-join-requests'),
   AcceptMtJoinRequestsJob: require('./accept-mt-join-requests'),
   AnnounceTrainingsJob: require('./announce-trainings'),
-  BackupJob: require('./backup'),
   CheckSuspensionsJob: require('./check-suspensions'),
   DiscordMessageJob: require('./discord-message'),
   FinishSuspensionJob: require('./finish-suspension'),
