999f5545f3e1d05bebb1a253b739ef1c12114c8d
guidojw <guidojwdejong@hotmail.com>
2021-03-29 19:36:47 +0200

    fix: add ExileService to exports

--- app/services/index.js
+++ app/services/index.js
@@ -4,6 +4,7 @@ module.exports = {
   BanService: require('./ban'),
   CatalogService: require('./catalog'),
   GroupService: require('./group'),
+  ExileService: require('./exile'),
   StatusService: require('./status'),
   SuspensionService: require('./suspension'),
   TrainingService: require('./training'),
