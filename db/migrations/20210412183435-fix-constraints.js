722a2e9f3c71d7e8f808bf9cb432e3a6455ced1b
guidojw <guidojwdejong@hotmail.com>
2021-04-12 19:11:31 +0200

    fix: change date columns defaultValue to NOW()

--- db/migrations/20210412183435-fix-constraints.js
+++ db/migrations/20210412183435-fix-constraints.js
@@ -1,36 +1,46 @@
 'use strict'
 
 module.exports = {
-  up: (queryInterface /* , Sequelize */) => {
+  up: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction(async t => {
-      return Promise.all([
+      await Promise.all([
         changeUserIdColumn(queryInterface, 'bans', 'author_id', true, t),
         changeUserIdColumn(queryInterface, 'bans', 'user_id', true, t),
         changeUserIdColumn(queryInterface, 'ban_cancellations', 'author_id', true, t),
         changeUserIdColumn(queryInterface, 'training_cancellations', 'author_id', true, t),
         changeUserIdColumn(queryInterface, 'trainings', 'author_id', true, t)
       ])
+
+      return queryInterface.changeColumn('bans', 'date', {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('NOW')
+      }, { transaction: t })
     })
   },
 
-  down: (queryInterface /* , Sequelize */) => {
-    return queryInterface.sequelize.transaction(t => {
-      return Promise.all([
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(async t => {
+      await Promise.all([
         changeUserIdColumn(queryInterface, 'bans', 'author_id', false, t),
         changeUserIdColumn(queryInterface, 'bans', 'user_id', false, t),
         changeUserIdColumn(queryInterface, 'ban_cancellations', 'author_id', false, t),
         changeUserIdColumn(queryInterface, 'training_cancellations', 'author_id', false, t),
         changeUserIdColumn(queryInterface, 'trainings', 'author_id', false, t)
       ])
+
+      return queryInterface.changeColumn('bans', 'date', {
+        type: Sequelize.DATE,
+        allowNull: false
+      }, { transaction: t })
     })
   }
 }
 
 async function changeUserIdColumn (queryInterface, tableName, columnName, up, transaction) {
   const attributes = await queryInterface.describeTable(tableName)
-  const attribute = attributes[columnName]
   return queryInterface.changeColumn(tableName, columnName, {
-    type: attribute.type,
+    type: attributes[columnName].type,
     allowNull: !up
   }, { transaction })
 }
