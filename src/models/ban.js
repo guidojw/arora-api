9c1d2a3e317818fd6e17bc006107a49b8a84dd49
guidojw <guidojwdejong@hotmail.com>
2021-04-12 04:09:58 +0200

    feat: update models

--- src/models/ban.js
+++ src/models/ban.js
@@ -2,10 +2,21 @@
 
 module.exports = (sequelize, DataTypes) => {
   const Ban = sequelize.define('Ban', {
-    userId: {
+    authorId: {
       type: DataTypes.BIGINT,
       allowNull: false,
-      field: 'user_id'
+      field: 'author_id'
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    },
+    duration: DataTypes.INTEGER,
+    groupId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      field: 'group_id'
     },
     reason: {
       type: DataTypes.STRING,
@@ -19,20 +30,10 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       field: 'role_id'
     },
-    date: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW
-    },
-    authorId: {
+    userId: {
       type: DataTypes.BIGINT,
       allowNull: false,
-      field: 'author_id'
-    },
-    groupId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      field: 'group_id'
+      field: 'user_id'
     }
   }, {
     tableName: 'bans'
@@ -45,15 +46,46 @@ module.exports = (sequelize, DataTypes) => {
         name: 'banId'
       }
     })
+    Ban.hasMany(models.BanExtension, {
+      foreignKey: {
+        allowNull: false,
+        name: 'banId'
+      },
+      as: 'extensions'
+    })
   }
 
   Ban.loadScopes = models => {
-    Ban.addScope('defaultScope', {
+    const endsAtLiteral = sequelize.literal(
+      'date + ' +
+      '("Ban".duration||\' milliseconds\')::INTERVAL + ' +
+      '(COALESCE(SUM(extensions.duration), 0)||\' milliseconds\')::INTERVAL'
+    )
+    const baseScope = {
+      attributes: {
+        include: [
+          [endsAtLiteral, 'endsAt']
+        ]
+      },
       where: { '$BanCancellation.id$': null },
       include: [{
         model: models.BanCancellation,
         attributes: []
-      }]
+      }, {
+        model: models.BanExtension,
+        as: 'extensions'
+      }],
+      group: ['Ban.id', 'extensions.id'],
+      subQuery: false
+    }
+
+    Ban.addScope('defaultScope', {
+      ...baseScope,
+      having: sequelize.literal(`${endsAtLiteral.val} > NOW()`)
+    })
+    Ban.addScope('finished', {
+      ...baseScope,
+      having: sequelize.literal(`${endsAtLiteral.val} <= NOW()`)
     })
   }
 
