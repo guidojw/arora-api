08b9caecc4c629276d48823a3663c78cb370b402
guidojw <guidojwdejong@hotmail.com>
2021-05-15 23:06:29 +0200

    feat: run TypeORM migrations on release

--- .buildkite/release.sh
+++ .buildkite/release.sh
@@ -11,5 +11,5 @@ fi
 cd /opt/docker/arora-api/$STAGE || exit
 docker-compose pull app
 docker-compose build app
-docker-compose run --rm app npx sequelize-cli db:migrate
+docker-compose run --rm app npx typeorm migration:run
 docker-compose up -d app
