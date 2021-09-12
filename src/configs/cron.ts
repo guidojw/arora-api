ac56d18b98c99f5b774da20fd375d5072f9f303b
guidojw <guidojwdejong@hotmail.com>
2021-09-12 20:34:00 +0200

    feat(AcceptJoinRequestsJob): increase frequency

--- src/configs/cron.ts
+++ src/configs/cron.ts
@@ -6,7 +6,7 @@ export interface CronConfig { expression: string, job: symbol, args?: any[] }
 
 const cronConfig: { [key: string]: CronConfig } = {
   acceptJoinRequestsJob: {
-    expression: '*/30 * * * *', // https://crontab.guru/#*/30_*_*_*_*
+    expression: '*/5 * * * *', // https://crontab.guru/#*/30_*_*_*_*
     job: TYPES.AcceptJoinRequestsJob,
     args: [1018818]
   },
