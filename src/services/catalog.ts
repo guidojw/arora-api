2037e49e9c05bee172bcf27bc7b3b57ef1fd3c0e
Guido de Jong <35309288+guidojw@users.noreply.github.com>
2023-01-15 03:36:21 +0100

    fix: use new Roblox API endpoints in CatalogService (#492)
    * fix: use new Roblox API endpoints in CatalogService
    
    * feat: include details in endpoint result

--- src/services/catalog.ts
+++ src/services/catalog.ts
@@ -1,9 +1,48 @@
-import axios from 'axios'
-import { injectable } from 'inversify'
+import { inject, injectable } from 'inversify'
+import BaseAPI from '@guidojw/bloxy/dist/client/apis/BaseAPI'
+import type { Client } from '@guidojw/bloxy'
+import type { RobloxManager } from '../managers'
+import { constants } from '../util'
+
+const { TYPES } = constants
 
 @injectable()
 export default class CatalogService {
-  public async getItems (queryString: string): Promise<object[]> {
-    return (await axios.get<object[]>(`https://search.roblox.com/catalog/json?${queryString}`)).data
+  @inject(TYPES.RobloxManager) private readonly robloxManager!: RobloxManager
+
+  public async getMusicAssets (queryString: string): Promise<unknown> {
+    const client = this.robloxManager.getAuthenticatedClient() ?? this.robloxManager.getClient()
+    const api = new CatalogAPI(client)
+    return await api.getMusicAssets(queryString)
+  }
+}
+
+class CatalogAPI extends BaseAPI {
+  public constructor (client: Client) {
+    super({
+      client,
+      baseUrl: 'https://apis.roblox.com/toolbox-service/'
+    })
+  }
+
+  public async getMusicAssets (queryString: string): Promise<unknown> {
+    const result = (await this.request({
+      requiresAuth: true,
+      request: {
+        path: `v1/marketplace/300?${queryString}`
+      },
+      json: true
+    })).body
+    return await this.getDetails(result.data.map((asset: { id: number }) => asset.id))
+  }
+
+  public async getDetails (assetIds: number[]): Promise<unknown> {
+    return (await this.request({
+      requiresAuth: true,
+      request: {
+        path: `v1/items/details?assetIds=${assetIds.join(',')}`
+      },
+      json: true
+    })).body
   }
 }
