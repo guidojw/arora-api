7731fdc0a919bda78f3d92527af09943d2993864
guidojw <35309288+guidojw@users.noreply.github.com>
2023-02-05 23:52:02 +0100

    fix: explicitly specify all class-validator validations

--- src/entities/ban-extension.ts
+++ src/entities/ban-extension.ts
@@ -1,34 +1,43 @@
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
 import { Expose, Type } from 'class-transformer'
+import { IsNotEmpty, IsNumber, IsString, MaxLength, ValidateIf, ValidateNested } from 'class-validator'
 import Ban from './ban'
-import { IsNotEmpty } from 'class-validator'
 
 @Entity({ name: 'ban_extensions' })
 export default class BanExtension {
   @Expose()
   @PrimaryGeneratedColumn()
+  @ValidateIf(banExtension => typeof banExtension.id !== 'undefined')
+  @IsNumber({ maxDecimalPlaces: 0 })
   public readonly id!: number
 
   @Expose({ name: 'author_id' })
   @Column('bigint', { name: 'author_id' })
+  @IsNumber({ maxDecimalPlaces: 0 })
   public authorId!: number
 
   @Expose({ name: 'ban_id' })
   @Column({ name: 'ban_id' })
+  @IsNumber({ maxDecimalPlaces: 0 })
   public banId!: number
 
   @Expose()
   @Column()
+  @IsNumber({ maxDecimalPlaces: 0 })
   public duration!: number
 
   @Expose()
   @Column({ length: 255 })
+  @IsString()
   @IsNotEmpty()
+  @MaxLength(255)
   public reason!: string
 
   @Expose()
   @Type(() => Ban)
   @ManyToOne(() => Ban, ban => ban.extensions, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'ban_id' })
+  @ValidateIf(banExtension => typeof banExtension.ban !== 'undefined')
+  @ValidateNested()
   public ban!: Ban
 }
--- src/entities/exile.ts
+++ src/entities/exile.ts
@@ -1,24 +1,32 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
-import { IsNotEmpty } from 'class-validator'
+import { IsDate, IsNotEmpty, IsNumber, IsString, MaxLength, ValidateIf } from 'class-validator'
 
 @Entity({ name: 'exiles' })
 export default class Exile {
   @PrimaryGeneratedColumn()
+  @ValidateIf(exile => typeof exile.id !== 'undefined')
+  @IsNumber({ maxDecimalPlaces: 0 })
   public readonly id!: number
 
   @Column('bigint', { name: 'author_id' })
+  @IsNumber({ maxDecimalPlaces: 0 })
   public authorId!: number
 
   @Column('timestamp with time zone', { default: () => 'NOW()' })
+  @IsDate()
   public date!: Date
 
   @Column({ name: 'group_id' })
+  @IsNumber({ maxDecimalPlaces: 0 })
   public groupId!: number
 
   @Column({ length: 255 })
+  @IsString()
   @IsNotEmpty()
+  @MaxLength(255)
   public reason!: string
 
   @Column('bigint', { name: 'user_id' })
+  @IsNumber({ maxDecimalPlaces: 0 })
   public userId!: number
 }
