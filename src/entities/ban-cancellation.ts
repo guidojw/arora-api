97d277fdfa62884867321cde4d87a829f5824a00
Guido de Jong <35309288+guidojw@users.noreply.github.com>
2023-02-06 00:00:18 +0100

    fix: explicitly specify all class-validator validations (#505)

--- src/entities/ban-cancellation.ts
+++ src/entities/ban-cancellation.ts
@@ -1,23 +1,31 @@
 import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm'
+import { IsNotEmpty, IsNumber, IsString, MaxLength, ValidateIf, ValidateNested } from 'class-validator'
 import Ban from './ban'
-import { IsNotEmpty } from 'class-validator'
 
 @Entity({ name: 'ban_cancellations' })
 export default class BanCancellation {
   @PrimaryGeneratedColumn()
+  @ValidateIf(banCancellation => typeof banCancellation.id !== 'undefined')
+  @IsNumber({ maxDecimalPlaces: 0 })
   public readonly id!: number
 
   @Column('bigint', { name: 'author_id' })
+  @IsNumber({ maxDecimalPlaces: 0 })
   public authorId!: number
 
   @Column({ name: 'ban_id' })
+  @IsNumber({ maxDecimalPlaces: 0 })
   public banId!: number
 
   @Column({ length: 255 })
+  @IsString()
   @IsNotEmpty()
+  @MaxLength(255)
   public reason!: string
 
   @OneToOne(() => Ban, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'ban_id' })
+  @ValidateIf(banCancellation => typeof banCancellation.ban !== 'undefined')
+  @ValidateNested()
   public ban!: Ban
 }
